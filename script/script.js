52366fedf9f15f7ec8bec6a1c6014bb5ba9890a0
LyndonTomas <61931218+LyndonTomas@users.noreply.github.com>
2024-01-28 18:59:32 +0800

    add: const variables

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// global variables
+const wordDisplay = document.querySelector(".word-display");
+const guessesTExt = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const hangmanImage = document.querySelector(".hangman-box img");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = gameModal.querySelector("button");
