bc31d8c09154d3b5c2c3826efc3e25464ffd3698
LyndonTomas <61931218+LyndonTomas@users.noreply.github.com>
2024-01-28 12:09:50 +0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,40 @@
+// word library
+
+const wordLibrary = [
+    {
+        word: "breeze",
+        hint: "A gentle and refreshing wind that rustles through leaves, bringing a sense of coolness on a warm day."
+    },
+    {
+        word: "quirk",
+        hint: "A peculiar or unexpected behavioral trait or mannerism that adds a touch of uniqueness to someone's personality."
+    },
+    {
+        word: "jolly",
+        hint: "Full of high spirits and good humor, creating an atmosphere of joy and laughter around."
+    },
+    {
+        word: "cheese",
+        hint: "A dairy product made from milk, known for its various textures and flavors, commonly used in cooking or as a standalone snack.",
+    },
+    {
+        word: "mellow",
+        hint: "Having a soft and gentle flavor or personality, free from harshness or intensity."
+    },
+    {
+        word: "jigsaw",
+        hint: "A puzzle consisting of various interlocking pieces that, when assembled correctly, form a complete picture."
+    },
+    {
+        word: "harmony",
+        hint: "The pleasing arrangement or combination of elements, creating a sense of balance and agreement."
+    },
+    {
+        word: "pancake",
+        hint: "A flat, round cake made from batter and cooked on a griddle or pan, often served with toppings like syrup or fruit."
+    },
+    {
+        word: "library",
+        hint: "A place where books and other informational materials are kept and can be borrowed, providing a resource for reading and research."
+    }
+];
